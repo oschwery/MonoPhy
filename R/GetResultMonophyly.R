672239613ce898cbbd76103e0a33dcbd51d91c02
oschwery <oschwery@vols.utk.edu>
2015-10-15 04:12:03 -0400

    styles and annotations

--- R/GetResultMonophyly.R
+++ R/GetResultMonophyly.R
@@ -3,25 +3,23 @@
 
 GetResultMonophyly <-
 function(solution, taxlevels='ALL') {
-    Allresults <- list()
-    if (taxlevels!='ALL' & class(taxlevels)!='numeric') {
+    Allresults <- list()  # create empty list to be filled
+    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # test format of taxlevels argument and display error if format is wrong
 	stop("taxlevels must be either 'ALL' or numeric!")
     }
-    if (taxlevels=='ALL') {
-        for (i in 1:length(solution)){
-            nameres <- paste('Taxlevel',i,sep='_')
-            tmp <- (solution[[i]]$result)
-            Allresults[[nameres]] <- tmp
+    if (taxlevels == 'ALL') {  # if all taxlevels are looked for
+        for (i in 1:length(solution)) {  # loop through all taxlevels
+            nameres <- paste('Taxlevel', i, sep='_')  # create namelabel for current taxlevel
+            tmp <- (solution[[i]]$result)  # extract result object from solution
+            Allresults[[nameres]] <- tmp  # add extracted table as sub-object to output list and label it with the appropriate taxlevel nr.
         }
-    } else {
-        if (taxlevels > length(solution)){
+    } else {  # if only a specific taxlevel is requested
+        if (taxlevels > length(solution)) {  # test whether requested taxlevel is among available ones and display error if not
             stop('Requested taxonomic level not available (less levels specified as analysis input)!')
         }
-        #for (j in 1:length(taxlevels)) {
-            nameres <- paste('Taxlevel', taxlevels, sep='_')
-            tmp <- (solution[[taxlevels]]$result)
-            Allresults[[nameres]] <- tmp
-        #}
+        nameres <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
+        tmp <- (solution[[taxlevels]]$result)  # extract result object from solution
+        Allresults[[nameres]] <- tmp  # add extracted list as sub-object to output list and label it with the appropriate taxlevel nr.
     }
-    Allresults
+    Allresults  # export output list
 }
