ae1af75d5e20f6a94f31fe64d92f5b5c60004ca7
oschwery <oschwery@vols.utk.edu>
2015-10-15 01:39:24 -0400

    styles and annotation

--- R/GetOutlierTaxa.R
+++ R/GetOutlierTaxa.R
@@ -3,25 +3,23 @@
 
 GetOutlierTaxa <-
 function(solution, taxlevels='ALL') {
-    alltaxa <- list()
-    if (taxlevels!='ALL' & class(taxlevels)!='numeric') {
+    alltaxa <- list()  # create empty list to be filled
+    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # test format of taxlevels argument and display error if format is wrong
 	stop("taxlevels must be either 'ALL' or numeric!")
     }
-    if (taxlevels=='ALL') {
-        for (i in 1:length(solution)){
-            nametax <- paste('Taxlevel',i,sep='_')
-            tmp <- solution[[i]]$OutlierTaxa
-            alltaxa[[nametax]] <- tmp
+    if (taxlevels == 'ALL') {  # if all taxlevels are looked for
+        for (i in 1:length(solution)) {  # loop through all taxlevels
+            nametax <- paste('Taxlevel', i, sep='_')  # create namelabel for current taxlevel
+            tmp <- solution[[i]]$OutlierTaxa  # extract sub-list of outlier taxa from solution
+            alltaxa[[nametax]] <- tmp # add extracted list as sub-object to output list and label it with the appropriate taxlevel nr.
         }
-    } else {
-        if (taxlevels > length(solution)){
+    } else {  # if only a specific taxlevel is requested
+        if (taxlevels > length(solution)) {  # test whether requested taxlevel is among available ones and display error if not
             stop('Requested taxonomic level not available (less levels specified as analysis input)!')
         }
-                #for (j in 1:length(taxlevels)) {
-            nametax <- paste('Taxlevel', taxlevels, sep='_')
-            tmp <- solution[[taxlevels]]$OutlierTaxa
-            alltaxa[[nametax]] <- tmp
-        #}
+		nametax <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
+        tmp <- solution[[taxlevels]]$OutlierTaxa  # extract sub-list of outlier taxa from solution
+        alltaxa[[nametax]] <- tmp  # add extracted list as sub-object to output list and label it with the appropriate taxlevel nr.
     }
-    alltaxa
+    alltaxa  # export output list
 }
