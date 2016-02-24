c36301cb5edaba0e26c51ff6efffca73ba4d47f2
oschwery <oschwery@vols.utk.edu>
2016-02-24 14:45:52 -0500

    accepts level names as input now, does not display it in all cases yet though...

--- R/GetSummaryMonophyly.R
+++ R/GetSummaryMonophyly.R
@@ -1,25 +1,34 @@
 # get summary only displayed out of the outputlist of AssessMonophyly
 # written by Orlando Schwery 2015
 
-GetSummaryMonophyly <-
+GetSummaryMonophyly2 <-
 function(solution, taxlevels='ALL') {
     Allsummaries <- list()  # create empty list to be filled
-    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # test format of taxlevels argument and display error if format is wrong
-		stop("taxlevels must be either 'ALL' or numeric!")
-    }
+#    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # test format of taxlevels argument and display error if format is wrong
+#		stop("taxlevels must be either 'ALL' or numeric!")
+#    }
     if (taxlevels == 'ALL') {  # if all taxlevels are looked for
         for (i in 1:length(solution)) {  # loop through all taxlevels
             namesum <- paste('Taxlevel', i, sep='_')  # create namelabel for current taxlevel
             tmp <- (solution[[i]]$summary)  # extract summary object from solution
             Allsummaries[[namesum]] <- tmp  # add extracted table as sub-object to output list and label it with the appropriate taxlevel nr.
         }
-    } else {  # if only a specific taxlevel is requested
+    }   # if only a specific taxlevel is requested
+    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # if named taxlevel requested
+        namesum <- paste('Taxlevel', taxlevels, sep='_')
+        tmp <- (solution[[taxlevels]]$summary)
+        Allsummaries[[namesum]] <- tmp
+    }
+    if (class(taxlevels) == 'numeric') {
         if (taxlevels > length(solution)) {  # test whether requested taxlevel is among available ones and display error if not
             stop('Requested taxonomic level not available (less levels specified as analysis input)!')
         }
         namesum <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
         tmp <- (solution[[taxlevels]]$summary)  # extract summary object from solution
         Allsummaries[[namesum]] <- tmp  # add extracted list as sub-object to output list and label it with the appropriate taxlevel nr.
+    }
+    if (length(Allsummaries) == 0) {
+      stop('No results found. Consider reviewing taxlevels argument.')
     }
     Allsummaries  # export output list
 }
