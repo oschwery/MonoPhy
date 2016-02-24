b58b3d054edb057e10ce313299464c564ca05ad7
oschwery <oschwery@vols.utk.edu>
2016-02-24 17:19:10 -0500

    as in summary, now takes header names but doesn't always display

--- R/GetIntruderTaxa.R
+++ R/GetIntruderTaxa.R
@@ -4,9 +4,6 @@
 GetIntruderTaxa <-
 function(solution, taxa=NULL, taxlevels='ALL') {
     alltaxa <- list()  # create empty list to be filled
-    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # test format of taxlevels argument and display error if format is wrong
-	stop("taxlevels must be either 'ALL' or numeric!")
-    }
     if (taxlevels == 'ALL') {  # if all taxlevels are looked for
         for (i in 1:length(solution)) {  # loop through all taxlevels
             nametax <- paste('Taxlevel', i, sep='_')  # create namelabel for current taxlevel
@@ -23,23 +20,42 @@ function(solution, taxa=NULL, taxlevels='ALL') {
                 alltaxa[[nametax]] <- alltaxa2  # add compiled invaders of this taxlevel as sub-list to outputlist, named after current taxlevel
             }
         }
-    } else {  # if only a specific taxlevel is requested
+    }  # if a specific taxlevel should be focused on
+    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # if named taxlevel requested
+      nametax <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
+      if (length(taxa) == 0) {  # pick all if no taxa specified
+          tmp <- solution[[taxlevels]]$IntruderTaxa   # extract sub-list of intruder taxa from solution
+          alltaxa[[nametax]] <- tmp  # add extracted list as sub-object to output list and label it with the appropriate taxlevel nr.
+      } else {  # display specific taxa if requested
+           alltaxa2 <- list()  # create empty list to be filled
+           for (itx in 1:length(taxa)) {  # loop through vector of taxon names
+              nametax2 <- taxa[itx]  # create label with name of invaded taxon first
+              tmp <- solution[[taxlevels]]$IntruderTaxa[[taxa[itx]]]  # extract invaders for this taxon from respective sub-object of solution
+              alltaxa2[[nametax2]] <- tmp  # add extracted invaders to output list, named after invaded taxon
+            }
+          alltaxa[[nametax]] <- alltaxa2  # add compiled invaders of this taxlevel as sub-list to outputlist, named after current taxlevel
+      }
+    }
+    if (class(taxlevels) == 'numeric') {
         if (taxlevels > length(solution)) {  # test whether requested taxlevel is among available ones and display error if not
             stop('Requested taxonomic level not available (less levels specified as analysis input)!')
         }
-		nametax <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
-		if (length(taxa) == 0) {  # pick all if no taxa specified
-			tmp <- solution[[taxlevels]]$IntruderTaxa   # extract sub-list of intruder taxa from solution
+		    nametax <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
+		    if (length(taxa) == 0) {  # pick all if no taxa specified
+			      tmp <- solution[[taxlevels]]$IntruderTaxa   # extract sub-list of intruder taxa from solution
             alltaxa[[nametax]] <- tmp  # add extracted list as sub-object to output list and label it with the appropriate taxlevel nr.
         } else {  # display specific taxa if requested
-			alltaxa2 <- list()  # create empty list to be filled
-            for (itx in 1:length(taxa)) {  # loop through vector of taxon names
-                nametax2 <- taxa[i]  # create label with name of invaded taxon first
+			       alltaxa2 <- list()  # create empty list to be filled
+             for (itx in 1:length(taxa)) {  # loop through vector of taxon names
+                nametax2 <- taxa[itx]  # create label with name of invaded taxon first
                 tmp <- solution[[taxlevels]]$IntruderTaxa[[taxa[itx]]]  # extract invaders for this taxon from respective sub-object of solution
                 alltaxa2[[nametax2]] <- tmp  # add extracted invaders to output list, named after invaded taxon
-            }
+              }
             alltaxa[[nametax]] <- alltaxa2  # add compiled invaders of this taxlevel as sub-list to outputlist, named after current taxlevel
         }
+      }
+    if (length(alltaxa) ==0) {
+      stop('No results found. Consider reviewing taxlevels argument.')
     }
     alltaxa  # export output list
 }
