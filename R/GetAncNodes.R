056c1bac68fc5f9631b557791f3741268533de97
oschwery <oschwery@vols.utk.edu>
2016-02-24 16:27:52 -0500

    as in summary, now takes header names but doesn't always display

--- R/GetAncNodes.R
+++ R/GetAncNodes.R
@@ -1,12 +1,9 @@
 #get list of MRCA nodes of desired genera only displayed out of the outputlist of AssessMonophyly
 # written by Orlando Schwery 2015
 
-GetAncNodes <-
+GetAncNodes2 <-
 function(solution, taxa=NULL, taxlevels='ALL') {
     allnodes <- list() 	# create empty list to be filled
-    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # test for wrong formats for taxlevels and display error if the case
-	stop("taxlevels must be either 'ALL' or numeric!")
-    }
     if (taxlevels == 'ALL') {  #if no specific taxlevel should be focused on
         for (i in 1:length(solution)){  # for every taxlevel in the present solution
             namenod <- paste('Taxlevel', i, sep='_')  # create namelabel for current taxlevel
@@ -18,18 +15,33 @@ function(solution, taxa=NULL, taxlevels='ALL') {
                 allnodes[[namenod]] <- tmp  # add the extracted table as sub-entry to output list, named according to taxlevel
             }
         }
-    } else {  # if a specific taxlevel should be focused on
+    }  # if a specific taxlevel should be focused on
+    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # if named taxlevel requested
+        namenod <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
+        namenod <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
+		    if (length(taxa) == 0){  # display all if no genera specified
+          tmp <- solution[[taxlevels]]$result[, "MRCA", drop=FALSE]  # extract MRCA column out of result table
+          allnodes[[namenod]] <- tmp  # add the extracted table as sub-entry to output list, named according to taxlevel
+        } else {  # display specific genera if specified
+          tmp <- solution[[taxlevels]]$result[taxa, "MRCA", drop=FALSE]  # extract MRCA column out of result table for specified taxa only
+          allnodes[[namenod]] <- tmp  # add the extracted table as sub-entry to output list, named according to taxlevel
+        }
+    }
+     if (class(taxlevels) == 'numeric') {
         if (taxlevels > length(solution)){  # test if taxlevel number exceeds number of present taxlevels in solution and display error if the case
             stop('Requested taxonomic level not available (less levels specified as analysis input)!')
         }
-		namenod <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
-		if (length(taxa) == 0){  # display all if no genera specified
-            tmp <- solution[[taxlevels]]$result[, "MRCA", drop=FALSE]  # extract MRCA column out of result table
-            allnodes[[namenod]] <- tmp  # add the extracted table as sub-entry to output list, named according to taxlevel
+		    namenod <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
+		    if (length(taxa) == 0){  # display all if no genera specified
+          tmp <- solution[[taxlevels]]$result[, "MRCA", drop=FALSE]  # extract MRCA column out of result table
+          allnodes[[namenod]] <- tmp  # add the extracted table as sub-entry to output list, named according to taxlevel
         } else {  # display specific genera if specified
-            tmp <- solution[[taxlevels]]$result[taxa, "MRCA", drop=FALSE]  # extract MRCA column out of result table for specified taxa only
-            allnodes[[namenod]] <- tmp  # add the extracted table as sub-entry to output list, named according to taxlevel
+          tmp <- solution[[taxlevels]]$result[taxa, "MRCA", drop=FALSE]  # extract MRCA column out of result table for specified taxa only
+          allnodes[[namenod]] <- tmp  # add the extracted table as sub-entry to output list, named according to taxlevel
         }
+    }
+    if (length(allnodes) ==0) {
+      stop('No results found. Consider reviewing taxlevels argument.')
     }
     allnodes  # return final list with MRCA nodes
 }
