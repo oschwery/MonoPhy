9fb4ce9b541633b034236ef47b0eef448ade40bb
oschwery <oschwery@vols.utk.edu>
2016-02-24 20:58:28 -0500

    as in summary, now takes header names but doesn't always display

--- R/GetOutlierTaxa.R
+++ R/GetOutlierTaxa.R
@@ -1,25 +1,31 @@
 #get list of outliers (genera) only displayed out of the outputlist of AssessMonophyly
 # written by Orlando Schwery 2015
 
-GetOutlierTaxa <-
+GetOutlierTaxa2 <-
 function(solution, taxlevels='ALL') {
     alltaxa <- list()  # create empty list to be filled
-    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # test format of taxlevels argument and display error if format is wrong
-	stop("taxlevels must be either 'ALL' or numeric!")
-    }
     if (taxlevels == 'ALL') {  # if all taxlevels are looked for
         for (i in 1:length(solution)) {  # loop through all taxlevels
             nametax <- paste('Taxlevel', i, sep='_')  # create namelabel for current taxlevel
             tmp <- solution[[i]]$OutlierTaxa  # extract sub-list of outlier taxa from solution
             alltaxa[[nametax]] <- tmp # add extracted list as sub-object to output list and label it with the appropriate taxlevel nr.
         }
-    } else {  # if only a specific taxlevel is requested
+    }  # if a specific taxlevel should be focused on
+    if (taxlevels != 'ALL' & class(taxlevels) != 'numeric') {  # if named taxlevel requested
+      nametax <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
+      tmp <- solution[[taxlevels]]$OutlierTaxa  # extract sub-list of outlier taxa from solution
+      alltaxa[[nametax]] <- tmp  # add extracted list as sub-object to output list and label it with the appropriate taxlevel nr.
+    }
+      if (class(taxlevels) == 'numeric') {
         if (taxlevels > length(solution)) {  # test whether requested taxlevel is among available ones and display error if not
             stop('Requested taxonomic level not available (less levels specified as analysis input)!')
         }
-		nametax <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
+		    nametax <- paste('Taxlevel', taxlevels, sep='_')  # create namelabel for current taxlevel
         tmp <- solution[[taxlevels]]$OutlierTaxa  # extract sub-list of outlier taxa from solution
         alltaxa[[nametax]] <- tmp  # add extracted list as sub-object to output list and label it with the appropriate taxlevel nr.
+    }
+    if (length(alltaxa) == 0) {
+      stop('No results found. Consider reviewing taxlevels argument.')
     }
     alltaxa  # export output list
 }
